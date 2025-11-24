37a2ae41741b32c5154d6e0288f54cdd9cf83cb1
Bahabratan <bakokerim8@gmail.com>
2025-11-24 20:17:16 +0500

    Delete src directory

--- src/main/java/com/example/demo/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// Репозиторий для работы с таблицей User в базе данных
-public interface UserRepository extends JpaRepository<User, Long> {}
